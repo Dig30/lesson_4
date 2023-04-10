7bb831c6d0fab23b73e4ae340dda570f56611092
Родин Юрий Александрович <derek.ass98@Mail.ru>
2023-04-10 20:37:07 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,34 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+int number = ParseNumber("Введите число: ");
+int result = SumDigits(number);
+OutputDisplay(result);
+
+
+
+
+
+int ParseNumber(string str)
+{
+    Console.WriteLine(str);
+    return int.Parse(Console.ReadLine());
+}
+
+int SumDigits(int number)
+{
+    int sum = 0;
+    while(number != 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum;
+}
+
+void OutputDisplay(int value)
+{
+    Console.WriteLine("Ответ = " + value);
+}
